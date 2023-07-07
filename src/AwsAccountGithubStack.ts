ddca8a89b80d3e853bf21a55a7e2cf74a0202a03
Matthew Benton <m@mattb.tech>
2023-07-07 15:48:00 +0100

    Set thumbprints

--- src/AwsAccountGithubStack.ts
+++ src/AwsAccountGithubStack.ts
@@ -22,6 +22,10 @@ export class AwsAccountGithubStack extends Stack {
       {
         url: "https://token.actions.githubusercontent.com",
         clientIds: ["sts.amazonaws.com"],
+        thumbprints: [
+          "6938fd4d98bab03faadb97b34396831e3780aea1",
+          "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+        ],
       }
     );
 
