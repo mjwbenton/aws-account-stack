68f0a7c26aa42bae99ea936b076d873a39138239
Matthew Benton <m@mattb.tech>
2023-09-08 17:31:39 +0100

    fix: forgot to new the terraform stack

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import { App } from "aws-cdk-lib";
 import { AwsAccountGithubStack } from "./AwsAccountGithubStack";
 import { AwsAccountIdentityCenterStack } from "./AwsAccountIdentityCenterStack";
 import { AwsAccountRolesStack } from "./AwsAccountRolesStack";
+import { AwsAccountTerraformStack } from "./AwsAccountTerraformStack";
 
 const app = new App();
 
 new AwsAccountGithubStack(app, "AwsAccountGithub");
 new AwsAccountIdentityCenterStack(app, "AwsAccountIdentityCenter");
 new AwsAccountRolesStack(app, "AwsAccountRoles");
+new AwsAccountTerraformStack(app, "AwsAccountTerraform");
