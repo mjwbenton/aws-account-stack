e81136420b0624c24e64a5745b2b88ba54ad3075
Matthew Benton <m@mattb.tech>
2023-09-08 17:16:21 +0100

    Add stack for terraform stuff

---
+++ src/AwsAccountTerraformStack.ts
@@ -0,0 +1,14 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
+
+export class AwsAccountTerraformStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new Bucket(this, "TerraformStateBucket", {
+      bucketName: "terraform-state",
+      versioned: true,
+    });
+  }
+}
