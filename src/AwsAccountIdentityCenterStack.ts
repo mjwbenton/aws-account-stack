41dd322f290a7f3974eeaeba4d8fd930a995d91d
Matthew Benton <m@mattb.tech>
2025-04-05 18:25:07 +0100

    Add new account to identity center setup

--- src/AwsAccountIdentityCenterStack.ts
+++ src/AwsAccountIdentityCenterStack.ts
@@ -5,6 +5,8 @@ import { Construct } from "constructs";
 const SSO_INSTANCE_ARN = "arn:aws:sso:::instance/ssoins-7223a6ee7aef14b8";
 const ADMIN_USER = "84c81448-4031-701c-3506-2e201feea745";
 
+const ACCOUNTS = ["858777967843", "552800114493"];
+
 export class AwsAccountIdentityCenterStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -20,13 +22,15 @@ export class AwsAccountIdentityCenterStack extends Stack {
       }
     );
 
-    new sso.CfnAssignment(this, "AdminAssignment", {
-      instanceArn: SSO_INSTANCE_ARN,
-      principalType: "USER",
-      principalId: ADMIN_USER,
-      permissionSetArn: adminPermissionsSet.attrPermissionSetArn,
-      targetType: "AWS_ACCOUNT",
-      targetId: this.account,
+    ACCOUNTS.forEach((account) => {
+      new sso.CfnAssignment(this, `AdminAssignment-${account}`, {
+        instanceArn: SSO_INSTANCE_ARN,
+        principalType: "USER",
+        principalId: ADMIN_USER,
+        permissionSetArn: adminPermissionsSet.attrPermissionSetArn,
+        targetType: "AWS_ACCOUNT",
+        targetId: account,
+      });
     });
   }
 }
