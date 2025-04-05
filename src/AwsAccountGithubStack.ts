13deab6ed383d7c9b78fd6e1f40b0ebc6334fa9e
Matthew Benton <m@mattb.tech>
2025-04-05 22:07:00 +0100

    Enable sts:TagSession on trusted accounts

--- src/AwsAccountGithubStack.ts
+++ src/AwsAccountGithubStack.ts
@@ -57,7 +57,7 @@ export class AwsAccountGithubStack extends Stack {
           statements: [
             new PolicyStatement({
               effect: Effect.ALLOW,
-              actions: ["sts:AssumeRole"],
+              actions: ["sts:AssumeRole", "sts:TagSession"],
               resources: accountIds.map(
                 (accountId) => `arn:aws:iam::${accountId}:role/cdk-*`
               ),
@@ -80,7 +80,7 @@ export class AwsAccountGithubStack extends Stack {
           statements: [
             new PolicyStatement({
               effect: Effect.ALLOW,
-              actions: ["sts:AssumeRole"],
+              actions: ["sts:AssumeRole", "sts:TagSession"],
               resources: accountIds.map(
                 (accountId) => `arn:aws:iam::${accountId}:role/*`
               ),
