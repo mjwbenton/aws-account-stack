2c2b524d22acc4f710f323649b5ee2cd4e5f63c0
Matthew Benton <m@mattb.tech>
2025-05-14 14:44:08 +0100

    Add callbacks and other fixes

--- src/AwsSSOStack.ts
+++ src/AwsSSOStack.ts
@@ -1,13 +1,14 @@
-import { Stack, StackProps } from "aws-cdk-lib";
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import {
   AccountRecovery,
-  CfnIdentityPool,
   UserPool,
   UserPoolClient,
   UserPoolDomain,
 } from "aws-cdk-lib/aws-cognito";
 import { StringParameter } from "aws-cdk-lib/aws-ssm";
 import { Construct } from "constructs";
+
+const CALLBACK_URLS = ["https://lonesome.mattb.tech"];
 
 export class AwsSSOStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -30,17 +31,32 @@ export class AwsSSOStack extends Stack {
 
     const userPoolClient = new UserPoolClient(this, "Client", {
       userPool,
-      generateSecret: false,
+      oAuth: {
+        callbackUrls: CALLBACK_URLS,
+        logoutUrls: CALLBACK_URLS,
+      },
     });
 
-    new StringParameter(this, "UserPoolIdParam", {
+    const userPoolIdParam = new StringParameter(this, "UserPoolIdParam", {
       parameterName: "/mattb-sso/user-pool-id",
       stringValue: userPool.userPoolId,
     });
 
-    new StringParameter(this, "UserPoolClientIdParam", {
-      parameterName: "/mattb-sso/user-pool-client-id",
-      stringValue: userPoolClient.userPoolClientId,
+    const userPoolClientIdParam = new StringParameter(
+      this,
+      "UserPoolClientIdParam",
+      {
+        parameterName: "/mattb-sso/user-pool-client-id",
+        stringValue: userPoolClient.userPoolClientId,
+      }
+    );
+
+    new CfnOutput(this, "UserPoolIdParamOutput", {
+      value: userPoolIdParam.parameterArn,
+    });
+
+    new CfnOutput(this, "UserPoolClientIdParamOutput", {
+      value: userPoolClientIdParam.parameterArn,
     });
   }
 }
